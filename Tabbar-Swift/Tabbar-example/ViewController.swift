f84ea8f8c7dc3767deea98f2758f3645d64d803a
Chand Basha <chandbasha.iosdeveloper@gmail.com>
2020-02-08 12:28:53 +0530

    TabbarViewController-upload

---
+++ Tabbar-Swift/Tabbar-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tabbar-example
+//
+//  Created by apple on 2/7/20.
+//  Copyright © 2020 Terasoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
